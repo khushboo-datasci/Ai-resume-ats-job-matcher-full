967686f3927cb81cf480417bb8236befb73f81a6
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-17 19:30:11 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 pip install --upgrade pip
 pip install -r requirements.txt
+# If using Pro plan / Metal build (system packages supported)
+sudo apt-get update
+sudo apt-get install -y tesseract-ocr poppler-utils
