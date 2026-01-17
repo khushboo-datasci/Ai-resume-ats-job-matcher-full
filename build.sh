f2f6a08824323b4b6b16507bba3d55df05c7c39a
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-17 22:14:17 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 # Upgrade pip
 pip install --upgrade pip
 
-# Install python dependencies
+# Install Python dependencies
 pip install -r requirements.txt
 
-# No need for sudo in Pro/Metal plan
-# tesseract + poppler-utils already available in Metal plan
 echo "✅ Dependencies installed"
