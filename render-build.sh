9776d6e07f62bc442704de162d6cc9d4411ac987
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-17 09:54:18 +0530

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y tesseract-ocr poppler-utils
+set -e
+
+apt-get update
+apt-get install -y tesseract-ocr poppler-utils
+
+pip install --upgrade pip
 pip install -r requirements.txt
