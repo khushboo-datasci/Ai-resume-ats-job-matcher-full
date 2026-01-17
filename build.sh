f47cdb4bf8670d8779d30d5aef8bc546e559cd6a
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-17 15:33:19 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "---- Installing Python dependencies ----"
 pip install --upgrade pip
 pip install -r requirements.txt
-
-echo "---- Downloading spaCy English model ----"
 python -m spacy download en_core_web_sm
-
-echo "---- Build completed ----"
