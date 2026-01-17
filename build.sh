d9f82792002f7301e563c763c5bc21a6da85a758
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-17 13:44:24 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "---- Updating system packages ----"
-sudo apt-get update -y
-
-echo "---- Installing Tesseract OCR ----"
-sudo apt-get install -y tesseract-ocr
-
-echo "---- Installing Poppler Utils (PDF to image) ----"
-sudo apt-get install -y poppler-utils
-
 echo "---- Installing Python dependencies ----"
 pip install --upgrade pip
 pip install -r requirements.txt
