8415085f72f8cf1d2187062b5bbb73716a6d5453
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-17 11:25:03 +0530

    app.py

--- app.py
+++ app.py
@@ -214,6 +214,6 @@ description="Upload your resume and paste the job description to calculate ATS s
 
 # ------------------ Launch ------------------
 iface.launch(
-server_name="0.0.0.0",
-server_port=int(os.environ.get("PORT", 5000))
+    server_name="0.0.0.0",
+    server_port=7860
 )
