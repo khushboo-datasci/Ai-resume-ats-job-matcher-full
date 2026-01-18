fa42dd1691ffd8c69e64de6e9aca723d84118958
khushboo kumari <khushboosingh8504095934@gmail.com>
2026-01-18 14:27:07 +0530

    app.py

--- app.py
+++ app.py
@@ -131,4 +131,5 @@ iface = gr.Interface(
 )
 
 # -------------------- LAUNCH --------------------
-iface.launch(server_name="0.0.0.0", server_port=7860)
+port = int(os.environ.get("PORT", 7860))
+iface.launch(server_name="0.0.0.0", server_port=port, share=False, debug=True)
